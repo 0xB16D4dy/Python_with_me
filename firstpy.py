b2285589326a5fbcd74a6b6b391a256100ec6b5e
BiGDaddy <phuocem201@gmail.com>
2021-07-19 17:53:18 +0700

    thêm giá trị nhập

--- firstpy.py
+++ firstpy.py
@@ -1,4 +1,5 @@
-x, y = input().split()
+#cách nhập cùng lúc nhiều biến
+x, y = input("nhập lần lượt x và y: ").split()
 print("hello")
 print("giá trị của x là ", x)
 print("giá trị của y là, ", y) 

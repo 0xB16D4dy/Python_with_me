8d29fbc4fe6bd6b8f4405321a65529346d3dad14
BiGDaddy <phuocem201@gmail.com>
2021-07-19 18:00:35 +0700

    2 cách nhap nhieu gia tri trong python

--- firstpy.py
+++ firstpy.py
@@ -3,3 +3,9 @@ x, y = input("nhập lần lượt x và y: ").split()
 print("hello")
 print("giá trị của x là ", x)
 print("giá trị của y là, ", y) 
+
+#cách nhập cùng lúc bằng  List comprehension 
+x,y,z =[int(x) for x in input("nhập các giá trị x, y, z: ").split()]
+print("x = ", x)
+print("y = ", y)
+print("z = ", z)

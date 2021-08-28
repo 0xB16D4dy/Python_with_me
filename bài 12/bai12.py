b168affde28475019615a58bbdc6d37e1440494d
BiGDaddy <phuocem201>
2021-08-28 21:37:47 +0700

    update

--- bài 12/bai12.py
+++ bài 12/bai12.py
@@ -1,4 +1,8 @@
+from os import write
+
+
 with open('input.inp', 'r', encoding='utf-8') as input:
     word = input.readlines()
-    string = ' '.join(word).replace('\n','')
-    print(string)
+    string = ''.join(word).replace('\n','')
+with open('output.out', 'a') as output:
+    output.write(string)
